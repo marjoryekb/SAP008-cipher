77f9c51f478ded4093ec61e85b60c98d51be17ce
marjorye <marjorye.mem@gmail.com>
2022-08-02 11:14:42 -0300

    ajuste de cor e espaco

--- src/cipher.js
+++ src/cipher.js
@@ -4,10 +4,14 @@ function encode(offset, string) {
     throw TypeError();
   }
 let cifrarTexto = "";
-for (let i = 0, letras = string.length; i < letras; i++){
+for (let i = 0; i < string.length; i++){
+  if (32 != string.charCodeAt(i)) {
   let tabela = ((string.charCodeAt(i) - 65 + offset) % 26) + 65;
   cifrarTexto += String.fromCharCode(tabela);
 }
+else
+        cifrarTexto += String.fromCharCode(32);
+    }
 return cifrarTexto;
 }
 
@@ -16,10 +20,14 @@ function decode (offset, string){
     throw TypeError();
   }
   let decifrarTexto ="";
-  for (let i = 0, letras = string.length; i < letras; i++){
+  for (let i = 0; i <string.length; i++){
+    if (32 != string.charCodeAt(i)) {
     let nAscii = ((string.charCodeAt(i) - 90 - offset) % 26) + 90;
     decifrarTexto += String.fromCharCode(nAscii);
   }
+  else
+  decifrarTexto += String.fromCharCode(32);
+}
   return decifrarTexto;
 }
 
